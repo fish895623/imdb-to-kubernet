f7806fa70dc74ea78def04d77e174a94d5a68c1c
fish895623 <dan990429@gmail.com>
2021-07-21 10:05:45 +0000

    remove iris

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 
-from IrisWeb import detect_iris
 from Transormer import predict, transformer
 from Transormer import VOCAB_SIZE, NUM_LAYERS, DFF, D_MODEL, NUM_HEADS, DROPOUT
 
@@ -17,27 +16,7 @@ model.load_weights("best_modela.h5")
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/iris")
-def iris():
-    return render_template("iris.html")
-
-
-@app.route("/iris", methods=["POST"])
-def iris_post():
-    a0 = request.form["a0"]
-    a1 = request.form["a1"]
-    a2 = request.form["a2"]
-    a3 = request.form["a3"]
-    result = detect_iris(float(a0), float(a1), float(a2), float(a3))
-    return render_template("iris.html", a0=a0, a1=a1, a2=a2, a3=a3, result=result)
-
-
-@app.route("/transformer")
+@app.route("/transformer", methods=["GET"])
 def transformer():
     return render_template("transformer.html")
 
@@ -52,8 +31,7 @@ def transformer_post():
 @app.route("/transformer/post", methods=["POST"])
 def transformer_POST_form():
     sentence = str(request.form["sentence"])
-    run = predict(sentence)
-    return run
+    return predict(sentence)
 
 
 if __name__ == "__main__":
